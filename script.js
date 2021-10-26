c933ec14176ce5dd2928743bd97a133e4a0ee8f9
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-10-26 17:45:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var model = "https://teachablemachine.withgoogle.com/models/8Pe5xH2JG/";
+
+function changemodel() {
+    var n = document.getElementById("modelInput").value;
+    model = n;
+    console.log(model);
+}
