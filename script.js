880c93a30719bcb9daa6e2649cfc83f5f91e2a4f
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-10-27 16:18:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var model = "https://teachablemachine.withgoogle.com/models/8Pe5xH2JG/";
+var model = "https://teachablemachine.withgoogle.com/models/8Pe5xH2JG/model.json";
 
 function changemodel() {
     var n = document.getElementById("modelInput").value;
     model = n;
     console.log(model);
+}
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90,
+});
+
+var camera = document.getElementById("camera");
+
+Webcam.attach("#camera", camera);
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5 version: ", ml5.version);
+
+var classifier = ml5.imageClassifier(model,modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded");
 }
