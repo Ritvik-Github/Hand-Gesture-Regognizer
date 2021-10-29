1e55a82e63b995fa94439ce07f48cae6d0b20391
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-10-29 15:51:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ Webcam.set({
     png_quality: 90,
 });
 
+var one_text = document.getElementById("one_text");
+var two_text = document.getElementById("two_text");
+
 var camera = document.getElementById("camera");
 
 Webcam.attach("#camera", camera);
@@ -29,4 +32,33 @@ var classifier = ml5.imageClassifier(model,modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded");
+}
+
+function speak() {
+    var synth = window.SpeechSynthesis;
+    var speak_data_1 = "the first prediction is " + preditction_1; 
+    var speak_data_2 = "and the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+    console.log("gotResults()");
+    if (error){
+    console.log(error);
+    } else {
+        console.log(results);
+        one_text.innerHTML = results[0].label;
+        two_text.innerHTML = results[1].label;
+        var prediction_1 = results[0].label;
+        var prediction_2 = results[1].label;
+        speak();
+        console.log(prediction_1);
+        console.log(prediction_2);
+    }
 }
